6606263b7e0ec3fd426b7a7e29ef1c117a3c342d
KWB <98502147+kimwoobyoung@users.noreply.github.com>
2023-09-18 09:12:03 +0900

    [level 2] Title: 올바른 괄호, Time: 7.63 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        int open = 0;
+        int close = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+            } else if (s.charAt(i) == ')') {
+                close++;
+            } if (open < close) {
+                return false;
+            } 
+                
+        } if (open == close) {
+            return true;
+        }
+        return false;
+            
+        }
+        
+    }
