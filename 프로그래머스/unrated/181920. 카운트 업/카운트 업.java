6a44b74b57a1fce7c41ef6ed8acd151980014ba4
KWB <98502147+kimwoobyoung@users.noreply.github.com>
2023-10-13 09:31:52 +0900

    [level unrated] Title: 카운트 업, Time: 2.30 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,12 +1,8 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[(end_num - start_num) + 1];
-        int count = start_num;
         
-        for(int i = 0; i < (end_num - start_num) + 1; i++) {
-            answer[i] += count;
-            count++;
-        }
-        return answer;
+        return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
