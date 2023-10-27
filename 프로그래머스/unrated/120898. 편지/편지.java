3a4e9419cad8b4abe56b106e527e0bd3d8831759
KWB <98502147+kimwoobyoung@users.noreply.github.com>
2023-10-27 09:16:32 +0900

    [level unrated] Title: 편지, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int a = message.length();
+        a *= 2;
+        answer = a;
+        return answer;
+    }
+}
